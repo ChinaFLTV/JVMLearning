7990bbb0ca2bbfb0d289bedaf10bd0d584a3ee6e
ChinaFLTV <236010069@qq.com>
2022-09-22 09:35:11 +0800

    2022-9-22 9:35-Day7

---
+++ src/main/java/Day7/MethodAreaTest.java
@@ -0,0 +1,45 @@
+package Day7;
+
+
+/*
+ ** @author LiGuanda
+ ** @date  AM 8:40:07
+ ** @description This is a description of MethodAreaTest
+ ** @params
+ ** @return
+ ** @since version-1.0  */
+
+
+/**
+ * @author Lenovo
+ */
+public class MethodAreaTest {
+
+
+    public static void main(String[] args) {
+
+        String str = "JuJingyi loves Dage";
+        try {
+
+            Thread.sleep(100000000);
+
+        } catch (InterruptedException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+
+    }
+
+
+}
+
+class Order {
+
+
+    public static int heihei = 1;
+    public static final int hehe = 2;
+
+
+}
