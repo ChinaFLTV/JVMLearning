0803e16125ea7ecb301f3f200c1864d6167e606c
ChinaFLTV <236010069@qq.com>
2022-09-23 17:50:38 +0800

    2022-9-23 17:50-Day8

---
+++ src/main/java/Day8/MethodAreaTest.java
@@ -0,0 +1,36 @@
+package Day8;
+
+/*
+ ** @author LiGuanda
+ ** @date  PM 4:42:52
+ ** @description This is a description of MethodAreaTest
+ ** @params
+ ** @return
+ ** @since version-1.0  */
+
+
+/**
+ * @author Lenovo
+ */
+public class MethodAreaTest {
+
+
+    public String name;
+    protected int age;
+    public static final String WIFE = "JuJingyi";
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public void marryTo(String husbandName) {
+
+        System.out.println("JuJingyi was married to " + husbandName + " !");
+
+    }
+
+
+}
