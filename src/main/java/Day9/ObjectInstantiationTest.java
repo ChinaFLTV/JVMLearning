35b3537a22768d947c0427749408c2c9fa9a0af3
ChinaFLTV <236010069@qq.com>
2022-09-24 09:30:29 +0800

    2022-9-24 9:30-Day9

---
+++ src/main/java/Day9/ObjectInstantiationTest.java
@@ -0,0 +1,27 @@
+package Day9;
+
+/*
+** @author LiGuanda
+** @date  AM 9:07:44
+** @description This is a description of ObjectInstantiationTest
+** @params
+** @return
+** @since version-1.0  */
+
+
+import Day6.User;
+
+/**
+ * @author Lenovo
+ */
+public class ObjectInstantiationTest {
+
+
+    public static void main(String[] args){
+
+
+        User user = new User();
+
+    }
+
+}
