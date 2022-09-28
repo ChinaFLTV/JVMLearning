9b4b95db7a168ca4d8ba810d19d467001ebb28db
ChinaFLTV <236010069@qq.com>
2022-09-28 08:48:34 +0800

    2022-9-28 8:48-Day13

---
+++ src/main/java/Day13/StringTable.java
@@ -0,0 +1,27 @@
+package Day13;
+
+/*
+ ** @author LiGuanda
+ ** @date  AM 8:04:21
+ ** @description This is a description of StringTable
+ ** @params
+ ** @return
+ ** @since version-1.0  */
+
+
+/**
+ * @author Lenovo
+ */
+public class StringTable {
+
+
+    public static void main(String[] args) {
+
+        String str = "JuJingyi loves Dage";
+        System.out.println(str);
+
+
+    }
+
+
+}
