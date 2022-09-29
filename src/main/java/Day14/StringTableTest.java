4e65ef98facb1b6e9ba480801244da7678b752ff
ChinaFLTV <236010069@qq.com>
2022-09-29 08:47:13 +0800

    2022-9-29 8:47-Day14

---
+++ src/main/java/Day14/StringTableTest.java
@@ -0,0 +1,25 @@
+package Day14;
+
+/*
+ ** @author LiGuanda
+ ** @date  AM 7:55:27
+ ** @description This is a description of StringTableTest
+ ** @params
+ ** @return
+ ** @since version-1.0  */
+
+
+/**
+ * @author Lenovo
+ */
+public class StringTableTest {
+
+
+    public static void main(String[] args) {
+
+        String str = new String("abcd");
+
+    }
+
+
+}
