76d0507758f8d597e43aad69760a0f55ff38d9a2
ChinaFLTV <236010069@qq.com>
2022-10-02 10:01:21 +0800

    2022-10-2 10:00-Day17

---
+++ src/main/java/Day17/GCRootsTest.java
@@ -0,0 +1,33 @@
+package Day17;
+
+/*
+ ** @author LiGuanda
+ ** @date  AM 8:44:13
+ ** @description This is a description of GCRootsTest
+ ** @params
+ ** @return
+ ** @since version-1.0  */
+
+
+/**
+ * @author Lenovo
+ */
+public class GCRootsTest {
+
+
+    public String str = "JuJingyi";
+    public static Integer inte = 666;
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Object object = new Object();
+        object = null;
+        Thread.sleep(2000000);
+
+
+    }
+
+
+}
