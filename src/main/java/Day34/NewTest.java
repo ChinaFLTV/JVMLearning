172c2b44d4df5ebcd075944fbdc9f0f1c07f16cd
ChinaFLTV <236010069@qq.com>
2022-10-28 15:52:16 +0800

    2022-10-28 15:52-Day34

---
+++ src/main/java/Day34/NewTest.java
@@ -0,0 +1,21 @@
+package Day34;
+
+/**
+ * @author LiGuanda
+ * @date PM 3:49:09
+ * @description This is a description of NewTest
+ * @since version-1.0
+ */
+
+
+public class NewTest {
+
+
+    public static void main(String[] args) {
+
+        int[][] i = new int[10][0];
+
+    }
+
+
+}
